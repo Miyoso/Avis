11f2ead3eba140b5e29c711e6fa7001e4fda93b3
Gabriel De Chiara <dechiara1u@etu.univ-lorraine.fr>
2025-10-13 09:47:26 +0200

    Initial Commit of Taupe Meubles

---
+++ www/Parametres.php
@@ -0,0 +1,14 @@
+<?php
+
+  // Parametres de configuration de la connexion
+  // -> permet de porter l'application en ne modifiant qu'une seule fois
+  //	les param�tres de connexions � un serveur MySQL 
+
+  // $host="127.0.0.1";
+  $host="db";
+  $user="root";
+  $pass="password";
+  $base="TaupeMeubles";
+  $id_user="0000";
+  
+?>
