14d9ca4c3e85dec8321c861df4f4ed5c12cdfb2d
MiyasaWp <151748657+MiyasaWp@users.noreply.github.com>
2025-10-15 11:35:31 +0200

    Retour au code de base Meuble.php

--- www/meubles.php
+++ www/meubles.php
@@ -1,24 +1,3 @@
 <?php
-// Ancien code (vulnérable) :
-// header('location: '.$_GET["p"]);
-
-// correction
-$target = $_GET['p'] ?? '';
-
-
-$target = str_replace(["\r", "\n", "\0"], '', $target);
-
-// décoder pour éviter %2f%2f etc.
-$target = rawurldecode($target);
-
-// rapide vérif : que c'est pas un truc bizarre
-$parts = parse_url($target);
-if ($target !== '' && strpos($target, '/') === 0 && strpos($target, '//') !== 0 && !isset($parts['scheme']) && !isset($parts['host'])) {
-    header('Location: ' . $target, true, 302);
-    exit;
-}
-
-
-header('Location: /', true, 302);
-exit;
-// fin de correction
+	header('location: '.$_GET["p"]);
+?>
