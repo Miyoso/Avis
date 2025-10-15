5187eef4889fd3e921db5eaa33dc70817e2e2603
MiyasaWp <151748657+MiyasaWp@users.noreply.github.com>
2025-10-15 11:56:15 +0200

    Update Buffet.php

--- www/Buffet.php
+++ www/Buffet.php
@@ -75,8 +75,29 @@
 									include("Fonctions.inc.php");
 									include("Donnees.inc.php");
 
-										$mysqli=mysqli_connect($host,$user,$pass) or die("Problème de création de la base :".mysqli_error());
-										mysqli_select_db($mysqli,$base) or die("Impossible de sélectionner la base : $base");
+                                // code vuln :
+                                // $mysqli=mysqli_connect($host,$user,$pass) or die("Problème de création de la base :".mysqli_error());
+                                // mysqli_select_db($mysqli,$base) or die("Impossible de sélectionner la base : $base");
+
+                                // correction
+                                // pas afficher d'erreur technique à l'utilisateur
+
+                                $mysqli = @mysqli_connect($host, $user, $pass);
+                                if (!$mysqli) {
+                                    error_log('DB connection failed: ' . mysqli_connect_error());
+                                    header('HTTP/1.1 500 Internal Server Error');
+                                    echo 'Serveur temporairement indisponible. Veuillez réessayer plus tard.';
+                                    exit;
+                                }
+
+                                if (!@mysqli_select_db($mysqli, $base)) {
+                                    error_log('DB select failed: ' . mysqli_error($mysqli));
+                                    header('HTTP/1.1 500 Internal Server Error');
+                                    echo 'Serveur temporairement indisponible. Veuillez réessayer plus tard.';
+                                    mysqli_close($mysqli);
+                                    exit;
+                                }
+                                // fin de correction
 										//echo "<a href='ajouterProd.php'>Ajouter une rubrique</a><br/>";
 										$result = query($mysqli,'select PRODUITS.id_prod as id,PRODUITS.Libelle as lib,PRODUITS.Photo as photo, PRODUITS.Descriptif as descr from PRODUITS,RUBRIQUES,APPARTIENT where APPARTIENT.id_prod = PRODUITS.id_prod and APPARTIENT.id_rub = RUBRIQUES.id_rub and libelle_rub = \'Buffet\'');
 
