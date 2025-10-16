fd3ea19c6081975bc9646febfe97b92bbee00cba
MiyasaWp <151748657+MiyasaWp@users.noreply.github.com>
2025-10-16 21:11:09 +0200

    Up Fonctininc

--- www/Fonctions.inc.php
+++ www/Fonctions.inc.php
@@ -1,9 +1,20 @@
 <?php
+function query($link, $query)
+{
+    // Ancien (vulnérable) :
+    // $resultat = mysqli_query($link, $query) or die("$query : " . mysqli_error($link));
 
-  function query($link,$query)
-  { 
-    $resultat=mysqli_query($link,$query) or die("$query : ".mysqli_error($link));
-	return($resultat);
-  }
-  
+    // Début Correction
+    $resultat = mysqli_query($link, $query);
+    if ($resultat === false) {
+        // Logger l'erreur complète pour l'admin
+        error_log('DB query failed: ' . mysqli_error($link) . ' -- Query: ' . $query);
+
+
+        return false;
+    }
+    // Fin Correction
+
+    return $resultat;
+}
 ?>
