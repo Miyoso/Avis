c032d07da8f828a7275ac5d39c1cec0e65ed7477
MiyasaWp <151748657+MiyasaWp@users.noreply.github.com>
2025-10-16 20:31:39 +0200

    retour

--- www/meubles.php
+++ www/meubles.php
@@ -1,31 +1,3 @@
 <?php
-// Ancien code (vulnérable) :
-// header('location: '.$_GET["p"]);
-
-// correction
-$target = $_GET['p'] ?? '';
-
-// empêcher l'injection d'en-têtes
-$target = str_replace(["\r", "\n", "\0"], '', $target);
-
-// décoder les encodages
-$target = rawurldecode($target);
-
-// refuser les URLs compl
-$parts = parse_url($target);
-
-$is_internal = $target !== ''
-    && strpos($target, '/') === 0
-    && strpos($target, '//') !== 0
-    && !isset($parts['scheme'])
-    && !isset($parts['host']);
-
-if ($is_internal) {
-    header('Location: ' . $target, true, 302);
-    exit;
-}
-
-
-header('Location: /', true, 302);
-exit;
-// fin de correction
+	header('location: '.$_GET["p"]);
+?>
