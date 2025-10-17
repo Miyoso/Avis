fe0ab9f2d34f14a52f688d91cf5ef49072ebdb32
Gabriel De Chiara <dechiara1u@etu.univ-lorraine.fr>
2025-10-17 21:39:07 +0200

    Add .gitignore and update database configuration to use environment variables

--- www/Parametres.php
+++ www/Parametres.php
@@ -4,11 +4,19 @@
   // -> permet de porter l'application en ne modifiant qu'une seule fois
   //	les param�tres de connexions � un serveur MySQL 
 
-  // $host="127.0.0.1";
-  $host="db";
-  $user="root";
-  $pass="password";
-  $base="TaupeMeubles";
-  $id_user="0000";
+//MODIF
+// $host="127.0.0.1";
+//  $host="db";
+//  $user="root";
+//  $pass="password";
+//  $base="TaupeMeubles";
+//  $id_user="0000";
+
+
+    $host=getenv("DB_HOST");
+    $user=getenv("DB_USER");
+    $pass=getenv("DB_PASSWORD");
+    $base=getenv("DB_NAME");
+    $id_user=getenv("DB_ID_USER");
   
 ?>
